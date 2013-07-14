c776072e1e85a4c0dfa9401a30dab0f60525bf1e
Pedro Marin <plmarin@gmail.com>
2013-07-14 18:31:45 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var fs = require('fs');
-var index = fs.readFileSync('index.html');
+//var index = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+//app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
+fs.readFile('index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+app.get('/', function(request, response) {
+//    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+//    }).listen(80);
+});
 });
 
 var port = process.env.PORT || 5000;
